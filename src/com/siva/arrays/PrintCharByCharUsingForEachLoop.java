aa83355403505d32ffe50ffd6349bfef3298faf5
Sivakumar U <itsmesivakumar21@gmail.com>
2025-01-28 17:22:18 -0500

    Refactored the code

--- src/com/siva/arrays/PrintCharByCharUsingForEachLoop.java
+++ src/com/siva/arrays/PrintCharByCharUsingForEachLoop.java
@@ -1,6 +1,6 @@
 package com.siva.arrays;
 
-// Directly we cannot apply for-each loop on string in Java.
+// Directly we cannot apply for-each loop on single string in Java.
 public class PrintCharByCharUsingForEachLoop {
 
 	public static void main(String[] args) {
@@ -10,7 +10,7 @@ public class PrintCharByCharUsingForEachLoop {
 		 * for(char ch :str) { System.out.println(ch); } // It gives error.It works in array of strings.
 		 */
 
-		char[] arr = str.toCharArray();// here we are converting String into Array.
+		char[] arr = str.toCharArray();// here we are converting String into char Array.
 		for (char ch : arr)
 			System.out.println(ch);
 
